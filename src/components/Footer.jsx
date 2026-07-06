d4955035502495e5ff4fef3e4c7c9494dc07c8a8
Chandhru <64609696+Chandhru008@users.noreply.github.com>
2026-07-07 01:46:02 +0530

    fix(src/components/Footer.jsx): 2 UX fixes [Heuristic 4, Heuristic 8]

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer({ className, style }) {
   return (
-    <footer className={`site-footer ${className || ''}`} style={style}>
+<footer className={`site-footer ${className || ''}`} style={style}>
       <p>© {new Date().getFullYear()} Chandhru Kusalavan. All rights reserved.</p>
     </footer>
   )
